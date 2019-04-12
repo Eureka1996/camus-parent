8280bf7cf1c9dff2129554f15077239d12540654
lifulong <moses@lifulongdeMacBook-Pro.local>
2019-04-12 15:19:06 +0800

    add log4j.xml

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#mvn clean package -Dmaven.test.skip=true
+mvn clean package -Dmaven.test.skip=true
 
 [ -d release ] && { rm -rf release; }
 mkdir release
@@ -14,12 +14,13 @@ do
     cp camus-example/target/camus-example-0.1.0-SNAPSHOT-shaded.jar $dir
     cp bin/functions.sh $dir
     cp bin/start.sh $dir
-    echo -e "#!/bin/bash\n\nexport HADOOP_HOME=/cluster/software/hadoop\nexport HADOOP_USER_NAME=work\nbash start.sh camus_${topic}.properties /camus/lock/action" > "$dir/start_${topic}.sh" 
-    echo -e "#!/bin/bash\n\nexport HADOOP_HOME=/cluster/software/hadoop\nexport HADOOP_USER_NAME=work\nbash start.sh camus_${topic}_init.properties /camus/lock/action" > "$dir/start_init_${topic}.sh" 
+    echo -e "#!/bin/bash\n\nexport HADOOP_HOME=/cluster/software/hadoop\nexport HADOOP_USER_NAME=work\nbash start.sh camus_${topic}.properties /camus/lock/${topic}" > "$dir/start_${topic}.sh" 
+    echo -e "#!/bin/bash\n\nexport HADOOP_HOME=/cluster/software/hadoop\nexport HADOOP_USER_NAME=work\nbash start.sh camus_${topic}_init.properties /camus/lock/${topic}" > "$dir/start_init_${topic}.sh" 
     chmod +x "$dir/start_${topic}.sh"
     chmod +x "$dir/start_init_${topic}.sh"
     cp "config/camus_${topic}.properties" $dir
     cp "config/camus_${topic}_init.properties" $dir
+    cp "config/log4j.xml" $dir
     cp "azkaban/camus_${topic}.job" $dir
     cd $dir
     zip "../${topic}.zip" *
